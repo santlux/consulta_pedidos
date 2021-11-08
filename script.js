35e9ec1d19488621868ec7d10b47e7ca92e29587
erwin_hc <santlux.backup@gmail.com>
2021-11-08 16:17:38 -0300

    16,17

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ onload = fetch("./data.csv").then(res => {
 
     })
 })
+//FITRO PEDIDOS 
+function pesquisaPedido() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("inputPedido");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tabelaDados");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+//FITRO NOMES 
+function pesquisaNome() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("inputNome");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tabelaDados");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[2];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
