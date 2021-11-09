fbefe64a1f58632fa483a1b13361526ece77a5b1
erwin_hc <santlux.backup@gmail.com>
2021-11-09 10:07:26 -0300

    10.07

--- script.js
+++ script.js
@@ -17,48 +17,40 @@ onload = fetch("./data.csv").then(res => {
         // console.log(m);
 
     })
-})
-//FITRO PEDIDOS 
-function pesquisaPedido() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("inputPedido");
+
+  });
+
+
+  /// FILTRAR MULTIPLAS COLUNAS
+  function searchTableColumns() {
+    // Declare variables 
+    var input, filter, table, tr, i, j, column_length, count_td;
+    column_length = document.getElementById('tabelaDados').rows[0].cells.length;
+    input = document.getElementById("inputSearch");
     filter = input.value.toUpperCase();
     table = document.getElementById("tabelaDados");
     tr = table.getElementsByTagName("tr");
-  
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    for (i = 1; i < tr.length; i++) { // except first(heading) row
+      count_td = 0;
+      for(j = 0; j < column_length; j++){ // except first column
+          td = tr[i].getElementsByTagName("td")[j];
+          /* ADD columns here that you want you to filter to be used on */
+          if (td) {
+            if ( td.innerHTML.toUpperCase().indexOf(filter) > -1)  {            
+              count_td++;
+            }
+          }
+      }
+      if(count_td > 0){
           tr[i].style.display = "";
-        } else {
+      } else {
           tr[i].style.display = "none";
-        }
       }
     }
+    
   }
-//FITRO NOMES 
-function pesquisaNome() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("inputNome");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tabelaDados");
-    tr = table.getElementsByTagName("tr");
-  
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[2];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  }
+/// COLORIR ROWS 
+
+
+
+
